75e7f1a2a72194bbde46d8221580a27678047b74
Heber Mattos <heber.mattos@ilegra.com>
2020-10-22 01:34:41 -0300

    refactor

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000
 const exphbs = require('express-handlebars');
 const compression = require('compression')
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer')
+const config = require('config'); 
 
 app.use(compression());
 app.engine('handlebars', exphbs());
@@ -19,11 +21,52 @@ app.get('/', (req, res) => {
 })
 
 app.post('/email', (req, res) => {
-  console.log('email')
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'feanorkun@gmail.com',
+      pass: config.get('Email.Senha')
+    }
+  });
+  
+  const mailOptions = { 
+    from: req.body.email,
+    to: config.get('Email.Destinatarios'),
+    subject: config.get('Email.Assunto'),
+    html: `<!DOCTYPE html>
+            <html>
+            <head>
+            </head>
+            <body>
+            <table>
+              <tr>
+                <td>Nome:</td>
+                <td>`+req.body.name+`</td>
+              </tr>
+              <tr>
+              <td>Telefone:</td>
+                <td>`+req.body.phone+`</td>
+              </tr>
+              <tr>
+                <td>Email:</td>
+                <td>`+req.body.email+`</td>
+              </tr>
+              <tr>
+              <td>Mensagem:</td>
+                <td>`+req.body.message+`</td>
+              </tr>
+            </table>
+            </body>
+            </html>`
+  };
+
+  transporter.sendMail(mailOptions);
+
   res.render('index', { layout: false })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log('app listening at http://localhost:${port}')
 })
 
