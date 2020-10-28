6ab4d47a15379d7681ddf4ebaad8b5b510e1d584
Heber Mattos <heber.mattos@ilegra.com>
2020-10-27 22:22:23 -0300

    gulp

---
+++ build/app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars');
+const compression = require('compression')
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer')
+const config = require('config'); 
+
+app.use(compression());
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.render('index', { layout: false })
+})
+
+app.post('/email', (req, res) => {
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'feanorkun@gmail.com',
+      pass: config.get('Email.Senha')
+    }
+  });
+  
+  const mailOptions = { 
+    from: req.body.email,
+    to: config.get('Email.Destinatarios'),
+    subject: "LOGAZ - CONTATO PELO SITE",
+    html: `<!DOCTYPE html>
+            <html>
+            <head>
+            </head>
+            <body>
+            <table>
+              <tr>
+                <td>Nome:</td>
+                <td>`+req.body.name+`</td>
+              </tr>
+              <tr>
+              <td>Telefone:</td>
+                <td>`+req.body.phone+`</td>
+              </tr>
+              <tr>
+                <td>Email:</td>
+                <td>`+req.body.email+`</td>
+              </tr>
+              <tr>
+              <td>Mensagem:</td>
+                <td>`+req.body.message+`</td>
+              </tr>
+            </table>
+            </body>
+            </html>`
+  };
+
+  transporter.sendMail(mailOptions);
+
+  res.render('index', { layout: false })
+})
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const { src, dest, series, parallel } = require('gulp');;
+const minifyCSS = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const purgecss = require('gulp-purgecss')
+const concatCss = require('gulp-concat-css')
+const clean = require('gulp-clean');
+const gulpCopy = require('gulp-copy');
+const install = require("gulp-install");
+const imagemin = require('gulp-imagemin');
+const htmlreplace = require('gulp-html-replace');
+const htmlmin = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+
+function createFolders() {
+    return src('*.*', { read: false })
+        .pipe(dest('./build'))
+};
+
+function cleanBuild() {
+    return src('build', { read: false })
+        .pipe(clean());
+};
+
+function copyServer() {
+    return src(['app.js'])
+        .pipe(gulpCopy('build', { prefix: 1 }))
+};
+
+function copyConfig() {
+    return src(['config/**/*'])
+        .pipe(gulpCopy('build/config', { prefix: 1 }))
+};
+
+function copyPackage() {
+    return src(['package.json'])
+        .pipe(gulpCopy('build/', { prefix: 1 }))
+};
+
+function copyAssets() {
+    return src(['./public/assets/**/*'])
+        .pipe(gulpCopy('./build/public/assets', { prefix: 1 }))
+};
+
+function css() {
+    return src('./public/css/*.css')
+        .pipe(purgecss({
+            content: ["./views/**/*.handlebars"]
+        }))
+        .pipe(concatCss('app.min.css'))
+        .pipe(minifyCSS())
+        .pipe(dest('./build/public/css'))
+};
+
+function js() {
+    return src('./public/js/*.js', { sourcemaps: true })
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(dest('./build/public/js', { sourcemaps: false }))
+};
+
+function installPackages() {
+    return src('./build/package.json', { sourcemaps: true })
+        .pipe(install({npm: '--production',}))
+};
+
+function images() {
+    return src('./public/assets/**/*')
+        .pipe(imagemin({
+            verbose: true
+        }))
+        .pipe(dest('./build/public/assets'))
+};
+
+function html() {
+    return src('./views/**/*.handlebars')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlreplace({
+            'css': 'css/app.min.css',
+            'js': 'js/app.min.js'
+        }))
+        .pipe(dest('./build/views'));
+}
+
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.html = html;
+
+exports.default = series(
+        createFolders, cleanBuild, 
+        parallel(copyServer, copyConfig, copyPackage, copyAssets), 
+        parallel(images, installPackages, css, js, html)
+    );
